53b5da7088c8d9ff5a86a9b8869fee08cb8afaf6
snigavig <snigavig@gmail.com>
2015-02-26 01:39:28 +0200

    lesson 4b part 2

--- app/src/androidTest/java/com/goodcodeforfun/sunshine/data/TestUriMatcher.java
+++ app/src/androidTest/java/com/goodcodeforfun/sunshine/data/TestUriMatcher.java
@@ -13,12 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.sunshine.app.data;
+package com.goodcodeforfun.sunshine.data;
 
+import android.content.UriMatcher;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
 import com.goodcodeforfun.sunshine.data.WeatherContract;
+import com.goodcodeforfun.sunshine.data.WeatherProvider;
 
 /*
     Uncomment this class when you are ready to test your UriMatcher.  Note that this class utilizes
@@ -43,16 +45,16 @@ public class TestUriMatcher extends AndroidTestCase {
         for each of the Uri types that our ContentProvider can handle.  Uncomment this when you are
         ready to test your UriMatcher.
      */
-//    public void testUriMatcher() {
-//        UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
-//
-//        assertEquals("Error: The WEATHER URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_DIR), WeatherProvider.WEATHER);
-//        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR), WeatherProvider.WEATHER_WITH_LOCATION);
-//        assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR), WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE);
-//        assertEquals("Error: The LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_LOCATION_DIR), WeatherProvider.LOCATION);
-//    }
+    public void testUriMatcher() {
+        UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
+
+        assertEquals("Error: The WEATHER URI was matched incorrectly.",
+                testMatcher.match(TEST_WEATHER_DIR), WeatherProvider.WEATHER);
+        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
+                testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR), WeatherProvider.WEATHER_WITH_LOCATION);
+        assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
+                testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR), WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE);
+        assertEquals("Error: The LOCATION URI was matched incorrectly.",
+                testMatcher.match(TEST_LOCATION_DIR), WeatherProvider.LOCATION);
+    }
 }
--- app/src/main/java/com/goodcodeforfun/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/goodcodeforfun/sunshine/data/WeatherProvider.java
@@ -112,14 +112,17 @@ public class WeatherProvider extends ContentProvider {
     static UriMatcher buildUriMatcher() {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
-
+        UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
-
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, "weather", WEATHER);
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, "weather/*", WEATHER_WITH_LOCATION);
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, "weather/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+        sURIMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, "location", LOCATION);
 
         // 3) Return the new matcher!
-        return null;
+        return sURIMatcher;
     }
 
     /*
